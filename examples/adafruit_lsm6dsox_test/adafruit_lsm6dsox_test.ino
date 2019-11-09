28f9aff11cf70758073e8424ea6baa61f866753c
siddacious <nospam187+github@gmail.com>
2019-11-08 16:49:37 -0800

    committing test

--- examples/adafruit_lsm6dsox_test/adafruit_lsm6dsox_test.ino
+++ examples/adafruit_lsm6dsox_test/adafruit_lsm6dsox_test.ino
@@ -19,32 +19,33 @@ void setup(void) {
   }
   Serial.println("LSM6DSOX Found!");
 
-//  //sox.setDataRate(LSM6DSOX_DATARATE_31_25_HZ);
-//  Serial.print("Data rate set to: ");
-//  switch (sox.getDataRate()) {
-//    case LSM6DSOX_DATARATE_1_HZ: Serial.println("1 Hz"); break;
-//    case LSM6DSOX_DATARATE_1_95_HZ: Serial.println("1.95 Hz"); break;
-//    case LSM6DSOX_DATARATE_3_9_HZ: Serial.println("3.9 Hz"); break;
-//    case LSM6DSOX_DATARATE_7_81_HZ: Serial.println("7.81 Hz"); break;
-//    case LSM6DSOX_DATARATE_15_63_HZ: Serial.println("15.63 Hz"); break;
-//    case LSM6DSOX_DATARATE_31_25_HZ: Serial.println("31.25 Hz"); break;
-//    case LSM6DSOX_DATARATE_62_5_HZ: Serial.println("62.5 Hz"); break;
-//    case LSM6DSOX_DATARATE_125_HZ: Serial.println("125 Hz"); break;
-//    case LSM6DSOX_DATARATE_250_HZ: Serial.println("250 Hz"); break;
-//    case LSM6DSOX_DATARATE_500_HZ: Serial.println("500 Hz"); break;
-//    case LSM6DSOX_DATARATE_1000_HZ: Serial.println("1000 Hz"); break;
-//  }
-//
+  sox.setAccelDataRate(LSM6DSOX_RATE_208_HZ);
+  Serial.print("Accelerometer data rate set to: ");
+  switch (sox.getAccelDataRate()) {
+    case LSM6DSOX_RATE_SHUTDOWN: Serial.println("0 Hz"); break;
+    case LSM6DSOX_RATE_12_5_HZ: Serial.println("12.5 Hz"); break;
+    case LSM6DSOX_RATE_26_HZ: Serial.println("26 Hz"); break;
+    case LSM6DSOX_RATE_52_HZ: Serial.println("52 Hz"); break;
+    case LSM6DSOX_RATE_104_HZ: Serial.println("104 Hz"); break;
+    case LSM6DSOX_RATE_208_HZ: Serial.println("208 Hz"); break;
+    case LSM6DSOX_RATE_416_HZ: Serial.println("416 Hz"); break;
+    case LSM6DSOX_RATE_833_HZ: Serial.println("833 Hz"); break;
+    case LSM6DSOX_RATE_1_66K_HZ: Serial.println("1.66 KHz"); break;
+    case LSM6DSOX_RATE_3_33K_HZ: Serial.println("3.33 KHz"); break;
+    case LSM6DSOX_RATE_6_66K_HZ: Serial.println("6.66 KHz"); break;
+    case LSM6DSOX_RATE_1_6_HZ_LP: Serial.println("1.6 Hz"); break;
+  }
 
-//
-//  //sox.setRange(LSM6DSOX_RANGE_2_G);
-//  Serial.print("Range set to: ");
-//  switch (sox.getRange()) {
-//    case LSM6DSOX_RANGE_2_G: Serial.println("+-2G"); break;
-//    case LSM6DSOX_RANGE_4_G: Serial.println("+-4G"); break;
-//    case LSM6DSOX_RANGE_8_G: Serial.println("+-8G"); break;
-//    case LSM6DSOX_RANGE_16_G: Serial.println("+-16G"); break;
-//  }
+
+
+//  sox.setAccelRange(LSM6DSOX_ACCEL_RANGE_2_G);
+  Serial.print("Range set to: ");
+  switch (sox.getAccelRange()) {
+    case LSM6DSOX_ACCEL_RANGE_2_G: Serial.println("+-2G"); break;
+    case LSM6DSOX_ACCEL_RANGE_4_G: Serial.println("+-4G"); break;
+    case LSM6DSOX_ACCEL_RANGE_8_G: Serial.println("+-8G"); break;
+    case LSM6DSOX_ACCEL_RANGE_16_G: Serial.println("+-16G"); break;
+  }
 
 }
 
