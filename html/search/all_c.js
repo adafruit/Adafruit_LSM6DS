6a23b61f7aca9f089368807754d934341cb19b73
Doxygen CI <ci-arduino@invalid>
2021-08-09 14:46:22 +0000

    Deploy docs to GitHub Pages from commit 1ea3bd2c77
    Commit: 1ea3bd2c77be022ea5c847029471f7bf57635ee6
    GitHub Actions run: 1113223979

--- html/search/all_c.js
+++ html/search/all_c.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['temp_5fsensor',['temp_sensor',['../class_adafruit___l_s_m6_d_s.html#a0f6efe7ac16022a96a0920ac1a3bf175',1,'Adafruit_LSM6DS']]],
-  ['temperature',['temperature',['../class_adafruit___l_s_m6_d_s.html#a288fdae09a43a72907dd408d467929c4',1,'Adafruit_LSM6DS']]]
+  ['temperature',['temperature',['../class_adafruit___l_s_m6_d_s.html#a288fdae09a43a72907dd408d467929c4',1,'Adafruit_LSM6DS']]],
+  ['temperature_5fsensitivity',['temperature_sensitivity',['../class_adafruit___l_s_m6_d_s.html#a18fc32886a2e4e908e760d41e623b4e7',1,'Adafruit_LSM6DS']]]
 ];
