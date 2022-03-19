bdc6dc47196195152306425adf68f221a501bfbf
Doxygen CI <ci-arduino@invalid>
2022-03-19 16:21:29 +0000

    Deploy docs to GitHub Pages from commit 5891751133
    Commit: 58917511330a0c88dbec1f4145bb7a336baebd6d
    GitHub Actions run: 2009143683

--- html/search/functions_5.js
+++ html/search/functions_5.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['enablei2cmasterpullups',['enableI2CMasterPullups',['../class_adafruit___l_s_m6_d_s3.html#aa09610ef4062d06769a3877050b7eb84',1,'Adafruit_LSM6DS3::enableI2CMasterPullups()'],['../class_adafruit___l_s_m6_d_s_l.html#ae4eb44a7da1bb7c0707d020ec86b1fdd',1,'Adafruit_LSM6DSL::enableI2CMasterPullups()'],['../class_adafruit___l_s_m6_d_s_o_x.html#a98ef3a8399e0b9de5392c579fb0d175f',1,'Adafruit_LSM6DSOX::enableI2CMasterPullups()']]],
   ['enablepedometer',['enablePedometer',['../class_adafruit___l_s_m6_d_s.html#a99ae12559e61d1cafa34bf2610ee6bd6',1,'Adafruit_LSM6DS']]],
-  ['enablewakeup',['enableWakeup',['../class_adafruit___l_s_m6_d_s.html#a6dff11953542dccd0cc5a101f486c110',1,'Adafruit_LSM6DS']]]
+  ['enablewakeup',['enableWakeup',['../class_adafruit___l_s_m6_d_s.html#aea3d2f66d222a1cf881c27273c8d5505',1,'Adafruit_LSM6DS']]]
 ];
--- html/search/functions_9.js
+++ html/search/functions_9.js
@@ -4,5 +4,6 @@ var searchData=
   ['setaccelrange',['setAccelRange',['../class_adafruit___l_s_m6_d_s.html#a6b3a7ec375debedaa4e0a4a633226180',1,'Adafruit_LSM6DS::setAccelRange()'],['../class_adafruit___l_s_m6_d_s_o32.html#abd8a4d6fcbd4db634fbc49261b4ab06d',1,'Adafruit_LSM6DSO32::setAccelRange()']]],
   ['setgyrodatarate',['setGyroDataRate',['../class_adafruit___l_s_m6_d_s.html#a15c6d58bc068a4bab4250b798f2544a5',1,'Adafruit_LSM6DS']]],
   ['setgyrorange',['setGyroRange',['../class_adafruit___l_s_m6_d_s.html#a4e81265a8c40f5e2ca2c699cb672d34d',1,'Adafruit_LSM6DS']]],
+  ['shake',['shake',['../class_adafruit___l_s_m6_d_s.html#a44a7fa75565e81c06a203fb2ff37e21b',1,'Adafruit_LSM6DS']]],
   ['status',['status',['../class_adafruit___l_s_m6_d_s.html#a7bb1bac7d61be9146fd5dbbd9062111c',1,'Adafruit_LSM6DS']]]
 ];
