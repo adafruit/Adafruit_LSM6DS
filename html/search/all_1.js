2c53bebe907364c8f57c6386e8df9f02c2fe6410
Doxygen CI <ci-arduino@invalid>
2022-04-18 18:21:57 +0000

    Deploy docs to GitHub Pages from commit a3592ed92d
    Commit: a3592ed92d5c74e6cc2c30985817d6fc8554e1c2
    GitHub Actions run: 2185277646

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -4,6 +4,7 @@ var searchData=
   ['accel_5fsensor',['accel_sensor',['../class_adafruit___l_s_m6_d_s.html#ad98ceb93ab8a410e7e624e0bc39e2cc8',1,'Adafruit_LSM6DS']]],
   ['accelerationavailable',['accelerationAvailable',['../class_adafruit___l_s_m6_d_s.html#a3e8c0f75690ba8cdaf077f597b7d662d',1,'Adafruit_LSM6DS']]],
   ['accelerationsamplerate',['accelerationSampleRate',['../class_adafruit___l_s_m6_d_s.html#a13da3e8541da50498926ea5b487a8236',1,'Adafruit_LSM6DS']]],
+  ['accelrangebuffered',['accelRangeBuffered',['../class_adafruit___l_s_m6_d_s.html#abd288241c48dc7f951af2547b783de21',1,'Adafruit_LSM6DS']]],
   ['accx',['accX',['../class_adafruit___l_s_m6_d_s.html#a76ea9cf3e3cdd0c82c124d673d2143bf',1,'Adafruit_LSM6DS']]],
   ['accy',['accY',['../class_adafruit___l_s_m6_d_s.html#a3c31c69d9f1dbffe64d5e603c4c13521',1,'Adafruit_LSM6DS']]],
   ['accz',['accZ',['../class_adafruit___l_s_m6_d_s.html#a1ac9b2183e2b0055445d4ae71ffb7974',1,'Adafruit_LSM6DS']]],
--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['begin_5fi2c',['begin_I2C',['../class_adafruit___l_s_m6_d_s.html#a95312ec3370e42b23aa5c7bf40f8b94a',1,'Adafruit_LSM6DS']]],
-  ['begin_5fspi',['begin_SPI',['../class_adafruit___l_s_m6_d_s.html#a9857831ed74ce4ebdfc04799c678b92f',1,'Adafruit_LSM6DS::begin_SPI(uint8_t cs_pin, SPIClass *theSPI=&amp;SPI, int32_t sensorID=0)'],['../class_adafruit___l_s_m6_d_s.html#a07705c4368889069c9ef55884fe96b4f',1,'Adafruit_LSM6DS::begin_SPI(int8_t cs_pin, int8_t sck_pin, int8_t miso_pin, int8_t mosi_pin, int32_t sensorID=0)']]]
+  ['begin_5fspi',['begin_SPI',['../class_adafruit___l_s_m6_d_s.html#ad1d858128c2ceb9dc0a38cf7cbf3605d',1,'Adafruit_LSM6DS::begin_SPI(uint8_t cs_pin, SPIClass *theSPI=&amp;SPI, int32_t sensorID=0, uint32_t frequency=1000000)'],['../class_adafruit___l_s_m6_d_s.html#ad6acb505429301dc94443adbb75fe2ba',1,'Adafruit_LSM6DS::begin_SPI(int8_t cs_pin, int8_t sck_pin, int8_t miso_pin, int8_t mosi_pin, int32_t sensorID=0, uint32_t frequency=1000000)']]]
 ];
